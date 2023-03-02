e2c67e204cbeaf5391f111f0ef8f9e754dbf5f5f
Vitali Rotteker <vitali.rotteker@posteo.de>
2023-03-02 21:15:17 +0100

    Complete Exercise 1: GuessInput

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log({ guess });
+        setGuess("");
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        maxLength={5}
+        pattern={"[a-zA-Z]{5}"}
+        onChange={(event) => setGuess(event.target.value)}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
