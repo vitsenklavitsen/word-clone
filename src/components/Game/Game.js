25028dd01580183e746df33b913743a243c75702
Vitali Rotteker <vitali.rotteker@posteo.de>
2023-03-03 10:35:39 +0100

    Remove unused imports

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,10 +1,9 @@
 import React from "react";
 
 import { WORDS } from "../../data";
-import { range, sample } from "../../utils";
+import { sample } from "../../utils";
 import GuessInput from "../GuessInput/GuessInput";
 import GuessResults from "../GuessResults/GuessResults";
-import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
